cbe0e85d16410c634a3297a74a323d3df889a4a3
justbbarnett <justb@inkery.com>
2018-04-13 15:22:39 -0500

    starting index... just starting.

--- index.js
+++ index.js
@@ -3,5 +3,8 @@ let inquirer = require ("inquirer")
 let Word = require("./word.js");
 let colors = require('colors');
 
+let wordsArr = ["stuff", "things", "more", "useless", "random"]
+let random = Math.floor((Math.random()*(wordsArr.length - 1)));
+let selectedWord = wordsArr[random];
 
-
+console.log(selectedWord)
